59be5ffbceed89173c534e03d9f3abaed633bbe4
saiyam0211 <saiyamkumar2007+git@gmail.com>
2026-05-08 14:37:16 +0530

    style: update orb color palette, increase container height, and disable hover intensity

--- src/components/Orb.tsx
+++ src/components/Orb.tsx
@@ -108,9 +108,9 @@ export default function Orb({
       return vec4(colorIn.rgb / (a + 1e-5), a);
     }
 
-    const vec3 baseColor1 = vec3(0.611765, 0.262745, 0.996078);
-    const vec3 baseColor2 = vec3(0.298039, 0.760784, 0.913725);
-    const vec3 baseColor3 = vec3(0.062745, 0.078431, 0.600000);
+    const vec3 baseColor1 = vec3(0.259, 0.008, 1.000);
+    const vec3 baseColor2 = vec3(1.000, 0.188, 0.608);
+    const vec3 baseColor3 = vec3(0.050, 0.000, 0.220);
     const float innerRadius = 0.6;
     const float noiseScale = 0.65;
 
--- src/pages/LandingNew.tsx
+++ src/pages/LandingNew.tsx
@@ -2,9 +2,9 @@ import Orb from '../components/Orb';
 
 export default function LandingNew() {
   return (
-    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+    <div style={{ width: '100%', height: '800px', position: 'relative' }}>
       <Orb
-        hoverIntensity={0.1}
+        hoverIntensity={0}
         rotateOnHover={true}
         hue={0}
         forceHoverState={false}
